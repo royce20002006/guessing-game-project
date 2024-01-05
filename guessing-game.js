f2f826260ce347ac4935eca8a4a4372334595808
royce <royce20002006@yahoo.com>
2024-01-04 19:27:02 -0600

    finished

--- guessing-game.js
+++ guessing-game.js
@@ -4,36 +4,85 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+let secretNumber;
+let numAttempts;
+let randomInRange = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return secretNumber =  Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 
 
-let secretNumber = 15;
 
 let checkGuess = num => {
+   
+    
     if (num > secretNumber) {
         console.log('Too High.');
+        numAttempts--;
         return false;
     }
     if (num < secretNumber) {
         console.log('Too low.');
+        numAttempts--
         return false;
     }
-    if (num === secretNumber) {
+    if (num === secretNumber) {-79
         console.log('Correct!');
         return true;
     }
 };
 
+let askLimit = () => {
+    rl.question('What is your limit ', (limit) => {
+        console.log(`You have ${limit} geusses.`)
+        numAttempts = limit;
+        askRange();
+    })
+}
+let askRange = () => {
+    
+    rl.question('Enter a minimum number ', (numMin) => {
+        rl.question('Enter a maximum number ', (numMax) => {
+            console.log(`I'm thinking of a number between ${numMin} and ${numMax}...`);
+            numMin = Number(numMin);
+            numMax = Number(numMax);
+            randomInRange(numMin, numMax)
+            askGuess();
+        })
+    })
+}
+let win = 1;
 let askGuess = () => {
+    if (numAttempts === 0) {
+        if (win >= 1) {
+            console.log(`you lost after ${win - 1} games. Better luck next time.`)
+            win = 1;
+        }
+        
+        return rl.close();
+    }
     rl.question('Enter a guess: ', (answer) => {
         if (checkGuess(Number(answer))){
-            console.log('You win!');
-            rl.close();
+            if (win > 0) console.log(`You win round ${win}!`)
+            else console.log('You win!');
+            win++;
+            rl.question(`best of ${win}? (y or n)`, (answer2) => {
+                if (answer2.toLowerCase() === 'y') {
+                    askLimit();
+                }
+                if (answer2.toLowerCase() === 'n') 
+                {
+                    return rl.close();
+                }
+            })
+             
         } 
         else askGuess();
     })
-        
+    
     
 }
+askLimit();
 
-askGuess();
